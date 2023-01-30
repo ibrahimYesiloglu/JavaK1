710a0b0d9f135fab948592ab97cd8eed33320c8f
ibrahimYesiloglu <cizgi.2d@gmail.com>
2023-01-30 17:21:58 +0100

    dritte aufgabe bestanden.

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,7 +4,14 @@ public class Student {
     public static void main(String[] args) {
 
 
+        System.out.println("Merhaba" +
+                " 1");
 
+
+        System.out.println("Merhaba 2");
+
+
+        System.out.println("Merhaba 3");
 
 
 
